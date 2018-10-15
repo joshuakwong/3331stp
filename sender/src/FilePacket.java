8860977fa7abac76439390c97072ad9f8bcc80b0
joshuakwong <tinghinjosuha.kwong@student.unsw.edu.au>
2018-10-15 19:23:28 +1100

    new class filepacket and listener

---
+++ sender/src/FilePacket.java
@@ -0,0 +1,68 @@
+
+public class FilePacket {
+	
+	private byte[] data;
+	private int startNum;
+	private int endNum;
+	private boolean sentFlag;
+	private boolean ackedFlag;
+	private int expAck;
+	
+	FilePacket(byte[] data, int startNum, int endNum) {
+		this.data = data;
+		this.startNum = startNum;
+		this.endNum = endNum;
+		this.sentFlag = false;
+		this.ackedFlag = false;
+		this.expAck = 0;
+	}
+
+	public byte[] getData() {
+		return data;
+	}
+
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+
+	public int getStartNum() {
+		return startNum;
+	}
+
+	public void setStartNum(int startNum) {
+		this.startNum = startNum;
+	}
+
+	public int getEndNum() {
+		return endNum;
+	}
+
+	public void setEndNum(int endNum) {
+		this.endNum = endNum;
+	}
+
+	public boolean isSentFlag() {
+		return sentFlag;
+	}
+
+	public void setSentFlag(boolean sentFlag) {
+		this.sentFlag = sentFlag;
+	}
+
+	public boolean isAckedFlag() {
+		return ackedFlag;
+	}
+
+	public void setAckedFlag(boolean ackedFlag) {
+		this.ackedFlag = ackedFlag;
+	}
+
+	public int getExpAck() {
+		return expAck;
+	}
+
+	public void setExpAck(int expAck) {
+		this.expAck = expAck;
+	}
+
+}
---
+++ sender/src/Listener.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Listener implements Runnable{
+
+	private static final int MAXSIZE = 1024;
+	private static DatagramSocket socket = null;
+	private static DatagramPacket incomingPacket = null;
+	private static byte[] incomingPayload = new byte[5000];
+	private static Object recvObj = null;
+	private static boolean exit = false;
+	
+
+
+	Listener(DatagramSocket socket){
+		this.socket = socket;
+	}
+	
+	
+	@Override
+	public void run(){
+		System.out.println("-----------------starting listener-----------------");
+		
+		int recvSeqNum = 0;
+		int recvAckNum = 0;
+		while (exit == false) {
+			try {
+				incomingPacket = new DatagramPacket(incomingPayload, MAXSIZE);
+				socket.receive(incomingPacket);
+				recvObj = STP.deserialize(incomingPayload);
+				
+				recvSeqNum = ((STP) recvObj).getSeqNum();
+				recvAckNum = ((STP) recvObj).getAckNum();
+				
+				for (int i=0; i<Sender.segments.length; i++) {
+					if (recvAckNum == Sender.segments[i].getExpAck()) {
+						Sender.segments[i].setAckedFlag(true);
+					}
+				}
+				exit = checkAllAck();
+				System.out.println("Listener: received ack " + recvAckNum);
+			} catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("-----------------ending listener-----------------");
+	}
+
+	public static boolean checkAllAck() {
+		for (int i=0; i<Sender.segments.length; i++) 
+			if (Sender.segments[i].isAckedFlag() == false) return false;
+		
+		return true;
+	}
+	
+	public static void setExit(boolean exit) {
+		Listener.exit = exit;
+	}
+	
+}
